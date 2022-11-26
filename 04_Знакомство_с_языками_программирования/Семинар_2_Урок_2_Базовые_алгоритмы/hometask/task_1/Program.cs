469f9d4857836bd5b0853dcbfb275e6e3f5b5cd5
Andrey <aspirant_kravtsov@mail.ru>
2022-11-27 00:07:26 +0300

    Added task_1 2nd seminar

---
+++ 04_Знакомство_с_языками_программирования/Семинар_2_Урок_2_Базовые_алгоритмы/hometask/task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int num = Prompt ("Введите трехзнчаное число ");
+int check = num/100;
+
+if (num > 0 && check > 0 && check <= 10) 
+{
+    int var = (num - num%10)%100/10;
+    Console.Write($"Второе число в номере - {var}");    
+}
+else if (num < 0 && check < 0 && check >= -10) 
+{
+    int var = -(num - num%10)%100/10;
+    Console.Write($"Второе число в номере - {var}");    
+}
+else Console.Write($"Число {num} не трехзначное, не обманывайте!");    
+
