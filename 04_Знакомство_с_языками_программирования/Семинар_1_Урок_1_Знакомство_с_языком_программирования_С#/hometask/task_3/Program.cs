05af09a8017a17422715ed240bdfb4bab1f6f2c9
Andrey <aspirant_kravtsov@mail.ru>
2022-11-26 22:47:30 +0300

    Added task3

---
+++ 04_Знакомство_с_языками_программирования/Семинар_1_Урок_1_Знакомство_с_языком_программирования_С#/hometask/task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Масштабируем: проверяем число на кратность другому числу (т.е. 2, 3 и тп на выбор)
+
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int number = Prompt ("Введите число ");
+int div = Prompt ("Введите делитель ");
+
+if (number%div == 0)
+{
+    Console.WriteLine($"Число {number} кратно {div}");
+}
+else Console.WriteLine($"Число {number} НЕ кратно {div}");
