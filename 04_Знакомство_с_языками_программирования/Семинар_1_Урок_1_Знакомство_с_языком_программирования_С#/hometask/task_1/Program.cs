6fbdb0a36c1c4966f7f2699ca54d2b6042246c0c
Andrey <aspirant_kravtsov@mail.ru>
2022-11-26 22:17:15 +0300

    Added hometask_1 for 1st seminar

---
+++ 04_Знакомство_с_языками_программирования/Семинар_1_Урок_1_Знакомство_с_языком_программирования_С#/hometask/task_1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 1: Напишите программу
+// , которая на вход принимает два числа и выдаёт
+// , какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Масштабируем: из заданного количества целых чисел выбираем наибольшее и наименьшее
+
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int count = Prompt ("Количество чисел ");
+int min = 0;
+int max = 0;
+int index = 1;
+
+
+
+while (index<=count)
+{
+    int value = Prompt ("Число №" + index);
+     if (index == 1)
+     {
+         min = value;
+         max = value;
+         index ++;
+     }
+    else if (value <= min)
+    {
+        min = value;
+        index ++;
+    }
+    else if (value >= max)
+    {
+        max = value;
+        index ++;
+    }
+    else index ++;
+}
+
+Console.WriteLine("MIN = " + min);
+Console.WriteLine("MAX = " + max);
