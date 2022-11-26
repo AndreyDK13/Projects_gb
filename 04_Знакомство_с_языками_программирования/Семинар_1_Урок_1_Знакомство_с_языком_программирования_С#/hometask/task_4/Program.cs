864725462f068c83c7913f857bd73dbf63d96c21
Andrey <aspirant_kravtsov@mail.ru>
2022-11-26 23:25:14 +0300

    Added task_4

---
+++ 04_Знакомство_с_языками_программирования/Семинар_1_Урок_1_Знакомство_с_языком_программирования_С#/hometask/task_4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Масштабируем: выводим все числа от 1 до number, кратные заданному
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int number = Prompt ("Введите число ");
+int div = Prompt ("Введите делитель ");
+int index = 0;
+int result = 0;
+
+if (div==0)
+{
+    Console.Write("Нулю все числа кратны будут..");
+}
+else if (number >= div)
+{
+        while (index<number)
+            {
+                index = index + 1;
+                if (index%div==0)
+                {
+                    result = index;
+                    Console.Write($"{result}, ");
+                }
+            }
+}
+else Console.Write($"Вряд ли найдется число от 1 до {number}, кратное {div}");
