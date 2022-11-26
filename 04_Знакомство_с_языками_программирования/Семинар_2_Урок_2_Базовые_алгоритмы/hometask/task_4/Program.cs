785efd709b98595d26097e7415df459c071adae8
Andrey <aspirant_kravtsov@mail.ru>
2022-11-27 00:46:47 +0300

    Added hometask_4 2d seminar

---
+++ 04_Знакомство_с_языками_программирования/Семинар_2_Урок_2_Базовые_алгоритмы/hometask/task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. Не использовать строки
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int num = Prompt ("Номер дня в неделе ");
+bool check = num > 0 && num <=7;
+
+if (check==true) 
+{
+    if (num%5==2 || num%5==1)
+    {
+        Console.Write("выходной =)");           
+    }
+    else
+    Console.Write("будний =(");    
+}
+else Console.Write("Это точно номер дня в неделе??");   
