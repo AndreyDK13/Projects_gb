1637a7ff495ae45fb1c41117399220d290b7704a
Andrey <aspirant_kravtsov@mail.ru>
2022-11-27 00:29:12 +0300

    Added hometask_3 2d seminar

---
+++ 04_Знакомство_с_языками_программирования/Семинар_2_Урок_2_Базовые_алгоритмы/hometask/task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. . Не использовать строки
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int num = Prompt ("Введите число ");
+int check = num/100;
+
+if (num > 0 && check > 0) 
+{
+    int var = (num-num%100)%1000/100;
+    Console.Write($"Третья цифра в числе - {var}");    
+}
+else if (num < 0 && check < 0) 
+{
+    int var = -(num-num%100)%1000/100;
+    Console.Write($"Третья цифра в числе - {var}");    
+}
+else Console.Write($"В числе {num} нет трех цифр!");    
+
