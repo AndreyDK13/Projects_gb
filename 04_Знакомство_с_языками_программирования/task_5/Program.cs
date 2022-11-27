d4f042a6f598a0eb8969f70980727f784523220a
Andrey <aspirant_kravtsov@mail.ru>
2022-11-27 11:13:46 +0300

    Added task_5 3d seminar

---
+++ 04_Знакомство_с_языками_программирования/task_5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 5*: Напишите программу
+// , которая генерирует последовательность случайных 
+// чисел из 10 элементов в диапазоне от 1 до 10, и подсчитывает
+// , сколько сгенерировалось чисел больше 5.
+
+
+int [] array = new int [10];
+int count = 0;
+
+for (int i = 0;i<array.Length;i++)
+{
+    int tmp = new Random().Next(1,10); 
+    array[i] = tmp;
+    Console.WriteLine($"{tmp}");
+    if (array [i] > 5)
+    {
+        count ++;
+    }
+}
+Console.WriteLine($"Count is {count}");
