46a068c21828958770d6bd0f2edd966191c2bade
Andrey <aspirant_kravtsov@mail.ru>
2022-12-03 10:49:31 +0300

    Added task 1 Seminar 4

---
+++ 04_Знакомство_с_языками_программирования/Семинар_4_Урок_4_Функции/task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Counter(int number)
+{
+    int count = 0;
+    while (number > 0)
+        {
+            count ++;
+            number /=10;
+        }
+    return count;
+}
+
+int number = Prompt ("Input number ");
+Console.WriteLine($"Count of digits in {number} = {Counter(number)}");
