2b2830b2ecdf7374c2b2c8cc30c40e14375de657
Andrey <aspirant_kravtsov@mail.ru>
2022-12-03 10:59:17 +0300

    Added task 2 seminar 4

---
+++ 04_Знакомство_с_языками_программирования/Семинар_4_Урок_4_Функции/task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход 
+// число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+
+
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Product(int number)
+{
+    int value = 1;
+   for (int i=1; i<=number; i++)
+   {
+        value = value * i;
+   }
+   return value;
+}
+
+int number = Prompt ("Input number ");
+Console.WriteLine($"Count of digits in {number} = {Product(number)}");
