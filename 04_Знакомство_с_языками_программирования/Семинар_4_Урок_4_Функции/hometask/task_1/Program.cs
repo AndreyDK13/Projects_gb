9db32efcd1fc57ce5ec53d2679cfeac8bc8e72d9
Andrey <aspirant_kravtsov@mail.ru>
2022-12-03 20:04:46 +0300

    Added hometask 1 seminar 4

---
+++ 04_Знакомство_с_языками_программирования/Семинар_4_Урок_4_Функции/hometask/task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// // Задача 1: 
+// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. Реализовать через функции.
+
+
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberInPower (int number, int power)
+{
+    int result = 1;
+    for (int i=1; i <= power; i++)
+    {
+        result *= number;
+    }
+    // Console.WriteLine($"{number} to the power of {power} is {result}");
+    return result;
+}
+
+int A = Prompt("Input A ");
+int B = Prompt("Input B ");
+Console.WriteLine($"{A} to the power of {B} is {NumberInPower(A, B)}");
