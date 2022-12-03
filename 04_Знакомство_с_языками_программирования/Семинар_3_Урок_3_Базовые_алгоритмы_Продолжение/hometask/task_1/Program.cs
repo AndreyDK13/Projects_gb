741d076e18b1108283035db52c740b5ef26c6b25
Andrey <aspirant_kravtsov@mail.ru>
2022-12-03 13:54:44 +0300

    Added hometask 1 seminar 3

---
+++ 04_Знакомство_с_языками_программирования/Семинар_3_Урок_3_Базовые_алгоритмы_Продолжение/hometask/task_1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 1
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Не использовать строки
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Reverse(int number)
+{
+    int unnumber = 0;
+    while (number > 0)
+    {
+        unnumber = unnumber*10 + number%10;
+        number = number/10;
+    }
+    return unnumber;
+}
+
+void Answer(int number)
+{
+    if(number == Reverse(number))
+    {
+        Console.WriteLine($"{number} is polindrome");
+    }
+    else Console.WriteLine($"{number} is not polindrome");
+}
+
+// int check(int number)
+// {
+//     if(number == Reverse(number))
+//     {
+//         return System.Console.WriteLine($"{number} is polindrome");
+//     }
+//     else return System.Console.WriteLine($"{number} is not polindrome");
+// }
+// check(number);
+
+int number = Prompt ("Input number ");
+Answer (number);
+
+
+
