edba2a1ccbba2c8a090fbc9850937b4f516ae2f3
Andrey <aspirant_kravtsov@mail.ru>
2022-12-03 20:21:17 +0300

    Added hometask 2 seminar 4

---
+++ 04_Знакомство_с_языками_программирования/Семинар_4_Урок_4_Функции/hometask/task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// // Задача 2: Напишите программу, 
+// которая принимает на вход число и выдаёт сумму цифр в числе. 
+// Реализовать через функции.
+
+
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumOfDigits (int number)
+{
+    int result = 0;
+    int num = number;
+    while (num > 0)
+    {
+        result += num%10;
+        num /= 10;
+    }
+    return result;
+}
+
+int number = Prompt("Input number ");
+Console.WriteLine($"Sum of dights in number {number} is {SumOfDigits(number)}");
