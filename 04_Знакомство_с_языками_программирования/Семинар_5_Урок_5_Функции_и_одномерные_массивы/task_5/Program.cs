193624862d15f12b6e131b530344dadc4e73acbe
Andrey <aspirant_kravtsov@mail.ru>
2022-12-04 11:54:00 +0300

    Added task_5 seminar 5

---
+++ 04_Знакомство_с_языками_программирования/Семинар_5_Урок_5_Функции_и_одномерные_массивы/task_5/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 4: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+
+int[] GenerateArray(int Length, int minRange, int maxRange)
+{
+
+    int[] array = new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minRange, maxRange + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+int Promt(String message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int[] Multi(int[] Array)
+{
+    int[] result = new int [Array.Length/2 + Array.Length%2];
+    result[result.Length - 1]=Array[result.Length - 1];
+    for (int i=0; i < Array.Length/2; i++)
+    {
+        result[i] = Array[i] * Array[Array.Length - 1 - i];
+    }
+    return result;
+}
+
+
+int Length = Promt("Input length of array ");
+int minArray = Promt("Input min of array ");
+int maxArray = Promt("Input max of array ");
+
+int[] array = GenerateArray(Length, minArray, maxArray);
+PrintArray(array);
+PrintArray(Multi(array));
+// System.Console.WriteLine($"Count of numbers is {Multi(array)}");
