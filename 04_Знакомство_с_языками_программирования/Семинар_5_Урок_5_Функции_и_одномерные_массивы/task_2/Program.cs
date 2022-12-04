4b51d4edca7e3f33b7e9f99caff8b3efd2891532
Andrey <aspirant_kravtsov@mail.ru>
2022-12-04 10:44:47 +0300

    Added 2d task 5 seminar

---
+++ 04_Знакомство_с_языками_программирования/Семинар_5_Урок_5_Функции_и_одномерные_массивы/task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// // Напишите программу замены элементов массива: 
+// положительные элементы замените на соответствующие 
+// отрицательные, и наоборот.
+// // [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArray(int length, int minRange, int maxRange)
+{
+    int[] numbers = new int[length];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(minRange, maxRange + 1);
+    }
+    return numbers;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int[] numbers = CreateArray(15,-10,10);
+System.Console.WriteLine("Generate array");
+PrintArray(numbers);
+for (  int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = -numbers [i];
+}
+System.Console.WriteLine("Аrray: ");
+PrintArray(numbers);
