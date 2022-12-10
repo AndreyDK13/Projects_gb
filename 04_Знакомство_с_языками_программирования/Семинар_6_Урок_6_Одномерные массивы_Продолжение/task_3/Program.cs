d421144da09c9337c204d6089d1e46aa2eecfdf2
Andrey <aspirant_kravtsov@mail.ru>
2022-12-10 11:07:38 +0300

    Added task 3 seminar 6

---
+++ 04_Знакомство_с_языками_программирования/Семинар_6_Урок_6_Одномерные массивы_Продолжение/task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая будет преобразовывать 
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+int Prompt(string message)
+{
+    Console.Write($"{message} >");              // Вывод приглашения
+    return Convert.ToInt32(Console.ReadLine()); // ввод числа
+}
+
+int[] ToBinary(int Arg1)
+{
+    int[] number=new int[8];
+    int count=1;
+    while (Arg1>0)
+    {
+        int Ostatok=Arg1%2;
+        number[number.Length-count]=Ostatok;
+        count++;
+        Arg1=Arg1/2;
+    }
+    return number;
+
+}
+void PrintList(int[] list)
+{
+    for (int i = 0; i < list.Length; i++)
+    {
+        System.Console.WriteLine(list[i]);
+    }
+}
+
+int A=Prompt("Введите число > ");
+PrintList(ToBinary(A));
