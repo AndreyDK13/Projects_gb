868f2bfa3e441d02de0997e2f14fd042384afb47
Andrey <aspirant_kravtsov@mail.ru>
2022-12-10 10:56:58 +0300

    Added 2nd task seminar 6

---
+++ 04_Знакомство_с_языками_программирования/Семинар_6_Урок_6_Одномерные массивы_Продолжение/task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+// Напишите программу, которая принимает на вход три числа 
+// и проверяет, может ли существовать треугольник 
+// с сторонами такой длины.
+
+int Promt(String message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool CheckSizes(int A, int B, int C)
+{
+    if (A+B>C)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int A = Promt ($"Сторона A = ");
+int B = Promt ($"Сторона B = ");
+int C = Promt ($"Сторона C = ");
+if (CheckSizes(A,B,C) && CheckSizes(B,A,C) && CheckSizes(C,B,A))
+{
+    Console.WriteLine ("Такой треугольник существует");
+}
+else 
+{
+    Console.WriteLine ("Такой треугольник НЕ существует");
+}
