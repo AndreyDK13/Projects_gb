e18f31ecaa3899ad2e90ce48bfd216c3a48abb6a
Andrey <aspirant_kravtsov@mail.ru>
2022-12-18 11:09:45 +0300

    Added task_5 seminar 9

---
+++ 04_Знакомство_с_языками_программирования/Семинар_9_Урок_9_Рекурсия/task_5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+bool Power2Check(int number)
+{
+    if (number==2)
+    {
+        return true;
+    }    
+    return (number%2==0) && Power2Check(number = number/2);
+}
+
+int number = Prompt("Input number ");
+System.Console.WriteLine(Power2Check(number));
