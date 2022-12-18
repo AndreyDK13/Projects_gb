ee97c6c619fcd11238d94fdd609ba25aee9b9d7b
Andrey <aspirant_kravtsov@mail.ru>
2022-12-18 10:51:29 +0300

    Added task_3 seminar 9

---
+++ 04_Знакомство_с_языками_программирования/Семинар_9_Урок_9_Рекурсия/task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// // Напишите программу, которая будет принимать 
+// на вход число и возвращать сумму его цифр. 
+// Использовать рекурсию.
+// // 453 -> 12
+// // 45 -> 9
+
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int SummNumbers(int number)
+{
+    if (number ==0)
+    {
+        return 0;
+    }
+    return number%10 + SummNumbers(number / 10);
+}
+
+int number = Prompt("Введите число ");
+System.Console.Write(SummNumbers(number));
